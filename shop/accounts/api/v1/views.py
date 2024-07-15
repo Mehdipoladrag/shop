6cadd444e319e7f8c4b7e56ed49404b2b955d767
Mehdi Poladrag <mehdipoladrag1382@example.com>
2024-07-15 15:48:26 +0330

    Update Project(19) finish documentation for Accounts app Apis

--- shop/accounts/api/v1/views.py
+++ shop/accounts/api/v1/views.py
@@ -1,7 +1,8 @@
 # Libraries
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 # Modules
 from accounts.models import CustomProfileModel, CustomUser
 from .serializers import (
@@ -23,6 +24,24 @@ class UserListApiView(generics.ListAPIView):
     queryset = CustomUser.objects.all().order_by("date_joined")
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+    
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for user list",
+        operation_description="We Can See a user information and user list in this api",
+        response = {
+            200: openapi.Response(
+                description="List of User",
+                schema=UserSerializer(many=True),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+    def get(self, request):
+        return self.list(request)
 
 
 class UserProfileListApiView(generics.ListAPIView):
@@ -32,6 +51,23 @@ class UserProfileListApiView(generics.ListAPIView):
     serializer_class = UserProfileSerializer
     permission_classes = [AllowAny]
 
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for userprofile list",
+        operation_description="We Can See a userprofile information and userprofile list in this api",
+        response = {
+            200: openapi.Response(
+                description="List of UserProfile",
+                schema=UserProfileSerializer(many=True),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["Profile Information"]
+    )
+    def get(self, request): 
+        return self.list(request)
 
 # DELETE DATA API
 
@@ -46,6 +82,25 @@ class UserDeleteApiView(generics.DestroyAPIView):
     lookup_field = "pk"
 
 
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Delete User",
+        operation_description="Delete UserInformation in this api",
+        response = {
+            200: openapi.Response(
+                description="Delete a UserProfile Information",
+                schema=UserDataSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
 class UserProfileDeleteApiView(generics.DestroyAPIView):
 
     "Delete Profile API"
@@ -55,7 +110,25 @@ class UserProfileDeleteApiView(generics.DestroyAPIView):
     lookup_field = "pk"
     permission_classes = [AllowAny]
 
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Delete UserProfile",
+        operation_description="Delete UserProfile Information in this api",
+        response = {
+            200: openapi.Response(
+                description="Delete a UserProfile Information",
+                schema=UserProfileSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["Profile Information"]
+    )
 
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
 # UPDATE DATA API
 
 
@@ -70,18 +143,92 @@ class UserUpdateApiView(generics.UpdateAPIView):
     permission_classes = [AllowAny]
     lookup_field = "pk"
 
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Update User",
+        operation_description="Update UserInformation in this api",
+        response = {
+            200: openapi.Response(
+                description="Update User",
+                schema=UserSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+
+    def put(self,request,*args, **kwargs):
+        return self.update(request,*args, **kwargs)
+    
+
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Update User",
+        operation_description="Update UserInformation in this api",
+        response = {
+            200: openapi.Response(
+                description="Update User",
+                schema=UserSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
 class UserProfileUpdateApiView(generics.UpdateAPIView):
     """
-    A class to update where we can send
-    our http request as Put or send a patch
+        A class to update where we can send
+        our http request as Put or send a patch
     """
 
     queryset = CustomUser.objects.all()
     serializer_class = UserProfileDataSerializer
     permission_classes = [AllowAny]
     lookup_field = "pk"
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Update UserProfile Information",
+        operation_description="We can Update UserProfile Information in this api",
+        response = {
+            200: openapi.Response(
+                description="Update Profile User",
+                schema=UserProfileSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["Profile Information"]
+    )
 
+    def put(self,request,*args, **kwargs):
+        return self.update(request,*args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Update UserProfile Information",
+        operation_description="We can Update UserProfile Information in this api",
+        response = {
+            200: openapi.Response(
+                description="Update Profile User",
+                schema=UserProfileSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["Profile Information"]
+    )
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
 # CREATE DATA API
 
@@ -95,7 +242,25 @@ class UserCreateApiView(generics.CreateAPIView):
     serializer_class = UserDataSerializer
     permission_classes = [AllowAny]
 
-
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Create User",
+        operation_description="Create User in this api",
+        response = {
+            200: openapi.Response(
+                description="Create a New User",
+                schema=UserSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+    
+    def post(self, request): 
+        return self.create(request)
+    
 # DETAIL DATA API
 
 
@@ -106,8 +271,46 @@ class UserDetailApiView(generics.RetrieveAPIView):
     permission_classes = [AllowAny]
 
 
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for user list",
+        operation_description="We Can See a user information and user Detail with Pk in this api",
+        response = {
+            200: openapi.Response(
+                description="Detail of User",
+                schema=UserSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["User Information"]
+    )
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+
 class ProfileDetailApiView(generics.RetrieveAPIView):
     queryset = CustomProfileModel.objects.all()
     serializer_class = UserProfileDataSerializer
     lookup_field = "pk"
     permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        operation_summary = "This is a endpoint for Profile Detail",
+        operation_description="We Can See a user information and userProfile Detail in this api",
+        response = {
+            200: openapi.Response(
+                description="Detail of UserProfile",
+                schema=UserProfileSerializer(),
+            ),
+            401: openapi.Response(
+                description="Authentication credentials were not provided."
+            ),
+            403: openapi.Response(description="Permission denied."),
+        },
+        tags=["Profile Information"]
+    )
+    def get(self, request, pk):
+        return self.retrieve(request, pk)

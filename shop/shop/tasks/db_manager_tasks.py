657ea3a0341832abf37bbaa65555eab5ce0ccdc3
Mehdi Poladrag <mehdipoladrag1382@example.com>
2024-08-11 19:44:13 +0330

    Create a Task - Resolve #3

--- shop/shop/tasks/db_manager_tasks.py
+++ shop/shop/tasks/db_manager_tasks.py
@@ -1,30 +1,55 @@
 from celery import shared_task
 from datetime import datetime, timedelta
+from decimal import Decimal
 import pytz
-from shop.models import Order, OrderItem, Transaction, Invoice
 import logging
+
+from shop.models import Order, OrderItem, Transaction, Invoice, Product
+
 
 logger = logging.getLogger(__name__)
 
-@shared_task(name="ClearExpiredOrdersAndRelatedData")
-def clear_expired_orders_and_related_data():
+@shared_task(name="Clear Expired Orders And Related Data")
+def ClearExpiredOrdersRelatedData():
+    """
+        This is a Task for orders which 
+        Orders and Transaction Expired And We need 
+        To Remove this Orders.
+    """
 
     expiration_time = datetime.now(tz=pytz.timezone("Asia/Tehran")) - timedelta(seconds=30)
     
     try:
-
-        orders_to_delete = Order.objects.filter(order_date__lt=expiration_time)
-        for order in orders_to_delete:
-
-            OrderItem.objects.filter(order=order).delete()
-
-            Invoice.objects.filter(order=order).delete()
-            Invoice.delete()
-            transactions = Transaction.objects.filter(invoice__in=Invoice.objects.filter(order=order))
-            transactions.delete()
-
-            order.delete()
         
+        orders_to_delete = Order.objects.filter(order_date__lt=expiration_time)
+        
+        for order in orders_to_delete:
+            OrderItem.objects.filter(order=order).delete()
+            invoices_to_delete = Invoice.objects.filter(order=order)
+            for invoice in invoices_to_delete:
+                Transaction.objects.filter(invoice=invoice).delete()
+                invoice.delete()
+            order.delete()
         logger.info("Expired orders and related data have been cleared successfully.")
     except Exception as e:
         logger.error(f"An error occurred while clearing expired data: {e}")
+    finally: 
+        logger.error(f"Data Not found")
+
+
+@shared_task(name="Clear Product Astronomical Price")
+def ClearProductAstronomicalPrice(price_threshold='50000000'):
+    try:
+        price_threshold = Decimal(price_threshold)
+        products_delete = Product.objects.filter(price__gt=price_threshold) # We Should add a Argument For this Task in panel Admin
+        count = products_delete.count()
+
+        if count > 0:
+            products_delete.delete()
+            logger.info(f"Products with very high prices have been successfully removed. The number was deleted: {count}.")
+        else:
+            logger.info("No products found with too high a price to remove.")
+
+    except Exception as e:
+        logger.error(f"An error occurred while clearing expired data: {e}")
+

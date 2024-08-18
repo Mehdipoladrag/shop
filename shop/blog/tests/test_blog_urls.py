9991f2e339327e45ecd4c7a6a6119d63faf91826
Mehdi Poladrag <mehdipoladrag1382@example.com>
2024-08-18 21:09:21 +0330

    Create a Test For Urls In Blog app - Resolve #9

---
+++ shop/blog/tests/test_blog_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from ..views import BlogPageView, BlogDetailView, CategoryDetailView
+
+
+class BlogUrlsTest(SimpleTestCase):
+    """
+        Test All Url For Blog App
+    """
+    def test_blog_list_url_reslove(self):
+        url = reverse('blog:bloglist1')
+        self.assertEqual(resolve(url).func.view_class,BlogPageView )
+
+    def test_blog_detail_url_reslove(self): 
+        url = reverse('blog:blogdetail1', kwargs={ 'slug' : 1 })
+        self.assertEqual(resolve(url).func.view_class,BlogDetailView )
+
+    def test_category_detail_url_reslove(self): 
+        url = reverse('blog:category_detail', kwargs={ 'slug_cat' : 1 })
+        self.assertEqual(resolve(url).func.view_class,CategoryDetailView )

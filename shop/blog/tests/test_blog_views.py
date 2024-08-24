dcd2ed3bb607e5f19ee6c7094039e6d358fe3df1
Mehdi Poladrag <mehdipoladrag1382@example.com>
2024-08-24 11:07:30 +0330

    wof: Create a Test With TestCase For Blog View - Resolve #10

---
+++ shop/blog/tests/test_blog_views.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from django.urls import reverse
+from ..models import Blogs, Category_blog
+from accounts.models import CustomUser
+
+class BlogPageViewTest(TestCase):
+    def setUp(self):
+
+        self.user = CustomUser.objects.create_user(username='testuser', password='12345')
+
+        self.category = Category_blog.objects.create(name='Test Category', slug_cat='test-category')
+ 
+        for i in range(3):
+            Blogs.objects.create(
+                username=self.user,
+                blog_name=f'Test Blog {i}',
+                blog_image='path/to/image.jpg',
+                category=self.category,
+                blog_description='This is a test blog.',
+                slug=f'test-blog-{i}'
+            )
+
+    def test_blog_page_view_status_code(self):
+        url = reverse('blog:bloglist1')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+
+class CategoryDetailViewTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='testuser', password='12345')
+        self.category = Category_blog.objects.create(name='Test Category', slug_cat='test-category')
+        self.blog = Blogs.objects.create(
+            username=self.user,
+            blog_name='Test Blog',
+            blog_image='path/to/image.jpg',
+            category=self.category,
+            blog_description='This is a test blog.',
+            slug='test-blog'
+        )
+
+    def test_category_creation(self):
+        category = Category_blog.objects.get(name='Test Category')
+        self.assertEqual(category.slug_cat, 'test-category')
+
+    def test_blog_creation(self):
+        blog = Blogs.objects.get(blog_name='Test Blog')
+        self.assertEqual(blog.username, self.user)
+        self.assertEqual(blog.category, self.category)
+        self.assertEqual(blog.slug, 'test-blog')
--- shop/blog/views.py
+++ shop/blog/views.py
@@ -16,6 +16,9 @@ class BlogPageView(ListView):
         context = super().get_context_data(**kwargs)
         context["bloglist"] = Blogs.objects.all().order_by("create_date")
         context["categories"] = Category_blog.objects.all()
+        
+        print("Context Data:", context)  # برای دیباگ
+        
         return context
 
 
